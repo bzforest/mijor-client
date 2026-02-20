26b699215c11a9045ea478837891d2780641b336
Anuson <anusornthongpaenchai@gmail.com>
2026-02-20 10:15:06 +0700

    feat(ui): add MenuLink and Tag ui

---
+++ mijor-client/components/ui/Tag.tsx
@@ -0,0 +1,33 @@
+interface TagProps {
+    label: string;
+    variant: "genre" | "language";
+}
+
+function Tag({ label, variant }: TagProps) {
+    return (
+        <div
+            className="
+                w-auto
+                px-[12px] py-[6px]
+                bg-brand-gray-100
+                rounded-[4px]
+            "
+        >
+            <span
+                className={`
+                    text-body-2-bold
+                    ${
+                        variant === "genre"
+                            ? "text-brand-gray-300"
+                            : "text-brand-gray-400"
+                    }
+                `}
+            >
+                {label}
+            </span>
+        </div>
+    );
+}
+
+
+export default Tag;
