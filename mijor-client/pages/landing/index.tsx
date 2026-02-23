0b637df2aa9f334883863be49d307b4693aaf030
Anuson <anusornthongpaenchai@gmail.com>
2026-02-23 14:08:32 +0700

    feat: implement initial landing page structure with basic sections.

---
+++ mijor-client/pages/landing/index.tsx
@@ -0,0 +1,19 @@
+function LandingPage() {
+    return (
+        <div>
+            <div id="search-section">
+            </div>
+
+            <div id="movie-list-section">
+            </div>
+
+            <div id="coupons-section">
+            </div>
+
+            <div id="cinemas-section">
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
