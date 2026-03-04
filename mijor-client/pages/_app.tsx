a60de41d57b929e4fc47b71cc118d9db652690e3
Best <bestsakditat@gmail.com>
2026-03-04 14:49:10 +0700

    feat(chatbot): implement AI chatbot component and roadmap documentation

--- mijor-client/pages/_app.tsx
+++ mijor-client/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { Roboto_Condensed } from "next/font/google";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { LocationProvider } from "@/contexts/LocationContext";
+import Chatbot from "@/components/common/Chatbot";
 
 const robotoCondensed = Roboto_Condensed({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <div className={`${robotoCondensed.variable} font-sans`}>
           <Navbar />
           <Component {...pageProps} />
+          <Chatbot />
         </div>
       </LocationProvider>
     </AuthProvider>
